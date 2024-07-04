82c87bdff72f848f56f1e602640677022c70a267
DWDW2 <daid8002@gmail.com>
2024-07-04 07:06:57 +0500

    UPDATE: implemented detailed page

--- frontend/app/testing/critical/page.tsx
+++ frontend/app/testing/critical/page.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+'use client'
+import useCritical from '@/hooks/useCritical'
+import React, {useEffect} from 'react'
 
 type Props = {}
 
-export default function page({}: Props) {
+export default function CriticalDetailed({}: Props) {
+  const {isLoading, fetchCriticalData, error, criticalData, criticalUrl, getAllCriticalTests, criticalDataAll} = useCritical()
+
+  const handleAnswer = () => {
+      return (answer: string) => {
+          console.log(answer)
+      }
+  }
+
+  const handleFetch = async () => {
+      await fetchCriticalData()
+      getAllCriticalTests()
+  }
+
+  useEffect(() => {
+      getAllCriticalTests()
+  }, []) 
+
+
+  if (isLoading) {
+      return(
+        <div>
+          loading
+        </div>
+      )
+  }
+  if (error) {
+      return <div>Error: ere</div>
+  }
+  console.log(criticalData, criticalUrl, criticalDataAll)
   return (
     <div>page</div>
   )
