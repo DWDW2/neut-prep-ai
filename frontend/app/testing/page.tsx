9baccd2579cdb32bb26c97fa05c750c1fb0f75b3
DWDW2 <daid8002@gmail.com>
2024-07-09 14:45:20 +0500

    UPDATE: added pie chart to dashboard page

--- frontend/app/testing/page.tsx
+++ frontend/app/testing/page.tsx
@@ -7,17 +7,46 @@ import Link from 'next/link'
 import Streak from '@/components/testing/dashboard/Streak';
 import TestStatistics from '@/components/testing/dashboard/TestStatistics';
 import Themes from '@/components/testing/dashboard/Themes';
+import { PieChartComponent } from '@/components/testing/dashboard/PieChart';
+import { ChartConfig } from '@/components/ui/chart';
 
 
 type Props = {}
 
 export default function Testing({}: Props) {
-  const skills = [
-    { name: 'Reading', points: 80 },
-    { name: 'Writing', points: 70 },
-    { name: 'Listening', points: 90 },
-    { name: 'Speaking', points: 60 },
-  ];
+  const chartData = [
+    { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
+    { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+    { browser: "firefox", visitors: 287, fill: "var(--color-firefox)" },
+    { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
+    { browser: "other", visitors: 190, fill: "var(--color-other)" },
+  ]
+  
+  const chartConfig = {
+    visitors: {
+      label: "Visitors",
+    },
+    chrome: {
+      label: "Chrome",
+      color: "hsl(var(--chart-1))",
+    },
+    safari: {
+      label: "Safari",
+      color: "hsl(var(--chart-2))",
+    },
+    firefox: {
+      label: "Firefox",
+      color: "hsl(var(--chart-3))",
+    },
+    edge: {
+      label: "Edge",
+      color: "hsl(var(--chart-4))",
+    },
+    other: {
+      label: "Other",
+      color: "hsl(var(--chart-5))",
+    },
+  } satisfies ChartConfig
     
   const performanceData = [
     { name: 'Critical Thinking', score: 45 },
@@ -25,7 +54,6 @@ export default function Testing({}: Props) {
     { name: 'Reading Comprehension', score: 35 },
     { name: 'Writing', score: 75 }
   ];
-  const bestSkills = skills.filter(skill => skill.points >= 70);
   
   const continueCourse = () => {
     alert('Continuing course...');
@@ -37,14 +65,15 @@ export default function Testing({}: Props) {
       <Streak initialStreak={5} lastLoginDate="2024-07-08" />
     </div>
     <div className="w-[40%] p-4 flex justify-end">
-      <TestStatistics
+      {/* <TestStatistics
         points={85}
         skills={skills}
         bestSkills={bestSkills}
         continueCourse={continueCourse}
-      />
+      /> */}
     </div>
   </div>
+  <PieChartComponent chartData={chartData} chartConfig={chartConfig} title='sdsadad' description='dsadsasd' nameKey='browser' dataKey='visitors' text='peeppp'/>
   <div className="p-4">
     <Themes performanceData={performanceData} />
   </div>
